75972d6d4fdd158da3753ea3b2fa55e83e9b90cf
nair-ian <163933244+nair-ian@users.noreply.github.com>
2025-09-29 10:46:40 -0300

    Add files via upload

---
+++ Liora/TelaCadastroUsuario.java
@@ -0,0 +1,97 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class TelaCadastroUsuario extends JFrame {
+    
+    private JTextField campoNome;
+    private JTextField campoCPF;
+    private JTextField campoTelefone;
+    private JTextField campoEmail;
+    private JTextField campoLogin;
+    private JPasswordField campoSenha;
+
+    private JButton botaoSalvar;
+    private JButton botaoLimpar;
+
+    public TelaCadastroUsuario() {
+        super("Cadastro de Usuário");
+
+        setLayout(new GridLayout(7, 2, 10, 10));
+
+        add(new JLabel("Nome:"));
+        campoNome = new JTextField();
+        add(campoNome);
+
+        add(new JLabel("CPF:"));
+        campoCPF = new JTextField();
+        add(campoCPF);
+
+        add(new JLabel("Telefone:"));
+        campoTelefone = new JTextField();
+        add(campoTelefone);
+
+        add(new JLabel("E-mail:"));
+        campoEmail = new JTextField();
+        add(campoEmail);
+
+        add(new JLabel("Login:"));
+        campoLogin = new JTextField();
+        add(campoLogin);
+
+        add(new JLabel("Senha:"));
+        campoSenha = new JPasswordField();
+        add(campoSenha);
+
+        
+        botaoSalvar = new JButton("Salvar");
+        botaoLimpar = new JButton("Limpar");
+
+        add(botaoSalvar);
+        add(botaoLimpar);
+
+        botaoSalvar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nome = campoNome.getText();
+                String cpf = campoCPF.getText();
+                String telefone = campoTelefone.getText();
+                String email = campoEmail.getText();
+                String login = campoLogin.getText();
+                String senha = new String(campoSenha.getPassword());
+
+                JOptionPane.showMessageDialog(null,
+                        "Usuário cadastrado com sucesso!\n\n" +
+                        "Nome: " + nome +
+                        "\nCPF: " + cpf +
+                        "\nTelefone: " + telefone +
+                        "\nE-mail: " + email +
+                        "\nLogin: " + login
+                );
+            }
+        });
+
+        botaoLimpar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                campoNome.setText("");
+                campoCPF.setText("");
+                campoTelefone.setText("");
+                campoEmail.setText("");
+                campoLogin.setText("");
+                campoSenha.setText("");
+            }
+        });
+
+        // Configurações da janela
+        setSize(400, 300);
+        setLocationRelativeTo(null); // centralizar na tela
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new TelaCadastroUsuario();
+    }
+}
